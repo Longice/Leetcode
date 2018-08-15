83a9c77d6c76a855e075b9c0ad377a6139cfac0c
Longice <12068001+Longice@users.noreply.github.com>
2018-08-15 09:55:12 +0800

    Update 024. Swap Nodes in Pairs.cpp

--- 024. Swap Nodes in Pairs.cpp
+++ 024. Swap Nodes in Pairs.cpp
@@ -35,3 +35,14 @@ public:
         return head;
     }
 };
+// solution 2
+ListNode* swapPairs(ListNode* head) {
+    ListNode **pp = &head, *a, *b;
+    while ((a = *pp) && (b = a->next)) {
+        a->next = b->next;
+        b->next = a;
+        *pp = b;
+        pp = &(a->next);
+    }
+    return head;
+}
