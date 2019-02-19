35cd83f01a07e3c37c4d731b9f8092a4f1140b14
Longice <12068001+Longice@users.noreply.github.com>
2019-02-19 10:24:13 +0800

    Update 03_1.cpp

--- 剑指offer/03_1.cpp
+++ 剑指offer/03_1.cpp
@@ -1,1 +1,32 @@
-
+// 找出数组中的重复数字
+// https://www.nowcoder.com/practice/623a5ac0ea5b4e5f95552655361ae0a8?tpId=13&tqId=11203&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+class Solution {
+public:
+    // Parameters:
+    //        numbers:     an array of integers
+    //        length:      the length of array numbers
+    //        duplication: (Output) the duplicated number in the array number
+    // Return value:       true if the input is valid, and there are some duplications in the array number
+    //                     otherwise false
+    bool duplicate(int numbers[], int length, int* duplication) {
+        if(numbers == nullptr || length == 0)
+            return false;
+        for(int i = 0; i < length; ++i){
+            if(numbers[i] < 0 || numbers[i] > length-1)
+                return false;
+        }
+        
+        for(int i = 0; i < length; ++i){
+            while(numbers[i] != i){
+                if(numbers[i] == numbers[numbers[i]]){
+                    *duplication = numbers[i];
+                    return true;
+                }
+                int tmp = numbers[numbers[i]];
+                numbers[numbers[i]] = numbers[i];
+                numbers[i] = tmp;
+             }
+        }
+        return false;
+    }
+};
